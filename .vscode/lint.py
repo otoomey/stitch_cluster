2c5b9881adc648258c9f291fa882abf5d99e2d07
otoomey <oscr.toomey@gmail.com>
2024-02-26 22:53:57 +0000

    Checked in build scripts

---
+++ .vscode/lint.py
@@ -0,0 +1,26 @@
+import re
+import sys
+import subprocess
+
+try:
+    subprocess.check_output([
+        'verilator', 
+        '--lint-only',
+        '-f',
+        './.vscode/linter.vc',
+        '--error-limit',
+        '9999',
+        '-Wall',
+        '-Wno-MULTITOP'
+    ], stderr=subprocess.STDOUT)
+except subprocess.CalledProcessError as e:
+    out = e.output.decode('ascii')
+
+out = re.sub(r'%.*\/\.bender\/[^%]*', '', out)
+
+if len(sys.argv) > 1:
+    pat = f'%.*{sys.argv[1]}[^%]*'
+    out = re.findall(pat, out)
+    out = '\n'.join(out)
+
+print(out, file=sys.stderr)
